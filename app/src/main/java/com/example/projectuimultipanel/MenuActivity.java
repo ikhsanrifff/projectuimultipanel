ab4d2bc105772fd7e0ff29ac827abae41ad79c75
ikhsanrifff <111258563+ikhsanrifff@users.noreply.github.com>
2022-09-10 13:25:42 +0700

    Initial commit

---
+++ app/src/main/java/com/example/projectuimultipanel/MenuActivity.java
@@ -0,0 +1,13 @@
+package com.example.projectuimultipanel;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MenuActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle SavedIntanceState) {
+        super.onCreate(SavedIntanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
